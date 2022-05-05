681977ae42174326660182a03ee380ee5abb48f6
Sarang Sami <sami@novin.dev>
2022-05-05 15:17:00 +0430

    feat: :art: Initializing Gatsby Project based on Hello World template
    Routings and error page test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
+import { Link } from "gatsby"
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <Link to="/nested/contact">Contact</Link>
+    Hello world!</div>
 }
