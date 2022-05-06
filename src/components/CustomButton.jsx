cf219340554f7e3957cd33542a3c5c8cf5c91f4e
Sarang Sami <sami@novin.dev>
2022-05-06 13:54:00 +0430

    feat: :zap: Styled-Components and Project Pages structures

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const CustomButton = styled.button`
+  background-color: purple;
+  color: white;
+  border-radius: 6px;
+  cursor: pointer;
+  padding:10px
+ 
+`
--- src/pages/points.jsx
+++ src/pages/points.jsx
@@ -1,13 +1,16 @@
 import { Link } from "gatsby"
 import React from "react"
-import Layout from "../components/Layout"
+import { CustomButton } from "../components/CustomButton"
+import Layout from '../Layout';
 
-export default function Home() {
+
+export default function Points() {
   return (
     <Layout>
       <Link to="/nested/contact">Contact</Link>
       <br/>
       <a href="https://www.gatsbyjs.com/">Global Non Gatsby Link</a>
+      <CustomButton>Test Button</CustomButton>
     </Layout>
   )
 }
